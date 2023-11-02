590055486c7f030e112892248429fd0b8ebc7a54
Kirill <terekhov-kirill000@mail.ru>
2023-11-02 20:23:25 +0300

    second changes

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -9,32 +9,53 @@ namespace pt = boost::property_tree;
 
 pt::ptree tree;
 
-    void AST(const int DetectUnattendedObjects, const int DetermineHumanOrCarObjects, const int DetermineNumOfHumansInGroup, const int DetermineObjectColor, const int DetermineOutliers, const int DetermineUnattendedObjectType, const int MaxObjectHeight, const int MaxObjectWidth, const int MaxUnattendedObjectHeight, const int MaxUnattendedObjectWidth, const int MinObjectHeight, const int MinObjectWidth, const int MinUnattendedObjectHeight, const int MinUnattendedObjectWidth, const int MotionDetectorSensitivity, const int SkipEqualFrames, const int TimeBeforeObjectIsLost, const int UnattendedDetectorSensitivity, const int UseColourInformation ) {
-        pt::ptree ASTtree;
-        ASTtree.add("DetectUnattendedObjects", DetectUnattendedObjects);
-        ASTtree.add("DetermineHumanOrCarObjects", DetermineHumanOrCarObjects);
-        ASTtree.add("DetermineNumOfHumansInGroup", DetermineNumOfHumansInGroup);
-        ASTtree.add("DetermineObjectColor", DetermineObjectColor);
-        ASTtree.add("DetermineOutliers", DetermineOutliers);
-        ASTtree.add("DetermineUnattendedObjectType", DetermineUnattendedObjectType);
-        ASTtree.add("MaxObjectHeight", MaxObjectHeight);
-        ASTtree.add("MaxObjectWidth", MaxObjectWidth);
-        ASTtree.add("MaxUnattendedObjectHeight", MaxUnattendedObjectHeight);
-        ASTtree.add("MaxUnattendedObjectWidth", MaxUnattendedObjectWidth);
-        ASTtree.add("MinObjectHeight", MinObjectHeight);
-        ASTtree.add("MinObjectWidth", MinObjectWidth);
-        ASTtree.add("MinUnattendedObjectHeight", MinUnattendedObjectHeight);
-        ASTtree.add("MinUnattendedObjectWidth", MinUnattendedObjectWidth);
-        ASTtree.add("MotionDetectorSensitivity", MotionDetectorSensitivity);
-        ASTtree.add("SkipEqualFrames", SkipEqualFrames);
-        ASTtree.add("TimeBeforeObjectIsLost", TimeBeforeObjectIsLost);
-        ASTtree.add("UnattendedDetectorSensitivity", UnattendedDetectorSensitivity);
-        ASTtree.add("UseColourInformation", UseColourInformation);
-
-        tree.add_child("root.AST", ASTtree);
+int check(const string& name_file) { //функция проверяет на наличие нужного файла проверка на путь. Функция которая будет проверять путь.
+    try {
+        if (std::ifstream(name_file)) {
+            return 0;
+        }
+        else {
+            cout << "File not find!!!";
+            return -1;
+        }
     }
-int main()
-{
-    AST(0,0,0,1,0,0,10000,10000,8000,8000,200,200,200,200,0,0,5,7,1);
-    write_xml("result.xml", tree);
+    catch (...) {
+        std::cout << "Failed !!!";
+        return -1;
+    }
 }
+
+int create_xml (const string& name_file) {
+
+    string line;
+
+    ifstream in(name_file); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            cout << line << endl;
+        }
+    }
+    in.close();
+    return 0;
+    }
+
+
+    int main(int argc, char* argv[])
+    {
+        if (argc < 1 || argc > 2) {
+            cout << "Error!";
+            return -1;
+        }
+
+        else {
+            const string name_file(argv[1]);
+            if (check(name_file) == -1) {
+                return -1;
+            }
+            else {
+                return create_xml(name_file);
+            }
+        }
+    }
