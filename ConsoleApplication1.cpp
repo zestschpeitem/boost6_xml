386a0f0e245dffc9a1ffb304d27c5083af8f5205
Kirill <terekhov-kirill000@mail.ru>
2023-11-13 21:47:16 +0300

    third changes

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 namespace pt = boost::property_tree;
 
 class XML_struct {
-    pt::ptree tree;
+    pt::ptree root;
 
     public:
     int check(const string& name_file) { //функция проверяет на наличие нужного файла проверка на путь. Функция которая будет проверять путь.
@@ -32,27 +32,48 @@ class XML_struct {
 
 
     int create_xml(const string& name_file) {
+
         string our_line;
         ifstream in(name_file); // окрываем файл для чтения
         vector<string> substrings;
+        string after_equally;
+        string type;
         pt::ptree AST;
         if (in.is_open())
         {
             while (getline(in, our_line))
             {
-                boost::split(substrings, our_line, boost::is_any_of("., ="), boost::token_compress_on);
+                boost::split(substrings, our_line, boost::is_any_of("."), boost::token_compress_on);
+                size_t pos = our_line.find("=");
+                if (pos++ != our_line.npos) {
+                    after_equally = our_line.substr(pos);
+                }
+                if (after_equally == "true" || after_equally == "false") {
+                    type = "bool";
+                }
+                else if(after_equally.find_first_not_of("0123456789") == string::npos){
+                    type = "int";
+                }
+                else {
+                    type = "string";
+                }
+
+                pt::ptree property;
                 for (auto& our_line : substrings) {
-                    if (our_line != "root" && our_line !="AST") {
-                        pt::ptree line;
-                        line.add("<xmlattr>.type", our_line);
-                        line.add("<xmlattr>.value", our_line);
-                        AST.add_child("AST", line);
+                    string my_line = our_line.substr(0, our_line.find("=", 0));
+                    if (our_line!="root" && our_line!="AST") {
+                    property.add("<xmlattr>.id", my_line);
+                    property.add("<xmlattr>.type", type);
+                    property.add("<xmlattr>.value", after_equally);
                     }
                 }
+                AST.add_child("property", property);
+                //каккую группу я сейчас заполняю узнать, если группа поменялась то аст записать в рут и создать новое поддерево аудио которое мы потом запишем в рут.
             }
+            root.add_child("root.AST", AST);
 
         }
-        tree.add_child("root", AST);
+
             in.close();
             return 0;
         }
@@ -61,7 +82,7 @@ class XML_struct {
 
 
     void WriteXML(const string& filename) {
-        write_xml(filename, tree);
+        write_xml(filename, root);
     }
 
 }; 
